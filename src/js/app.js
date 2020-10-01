76a3d383da50322bc6a01149ce76cc4df51c9c89
DESKTOP-G02G3LQ\artem <artem>
2020-10-01 23:28:29 +0300

    up

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,21 @@ import 'slick-carousel';
 $(document).ready(() => {
 	$('.js-hero-slider').slick({
 	  nextArrow: '.js-slider-next',
-	  prevArrow: '.js-slider-prev'
+	  prevArrow: '.js-slider-prev',
+	  responsive: [
+		{
+		  breakpoint: 900,
+		  settings: {
+			dots: true,
+			prevArrow: false,
+    		nextArrow: false
+		  }
+		}
+	  ]
 	})
 })
 
+$('.js-search').click(function(event) {
+	$(this).find('input').focus()
+
+})
